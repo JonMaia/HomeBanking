0a0d6ccc20a3c5f719bf919482df394c02cd5656
JonMaia <jonathannicolas.maia@gmail.com>
2019-10-29 10:31:40 -0300

    Implementación de "ErrorToFindUser" y algunos refactors en userSchema

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const ErrorHandler = require('../errors_response/error_handler');
+const ErrorToFindUser = require('../errors_response/error_to_find_user');
 const ErrorValidation = require('../errors_response/error_validation');
 
 module.exports = class UserController {
@@ -16,7 +17,10 @@ module.exports = class UserController {
 
     static async getUser(req, res){
         try {
-            const user = await User.find({id: req.params.id});
+            const user = await User.findOne({id: req.params.id});
+            if (!user) {
+                return ErrorHandler.handleError(res, new ErrorToFindUser());
+            }
             res.json(user);
         } catch(error){
             return ErrorHandler.handleError(res, new ErrorValidation(error.message));
