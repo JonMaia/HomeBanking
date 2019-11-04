14d63ec4e819152d89ced77fce736c5e14811dd0
JonMaia <jonathannicolas.maia@gmail.com>
2019-11-04 15:10:39 -0300

    Algunos refactors y correcciones

--- src/test/user_test.js
+++ src/test/user_test.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = 'test';
 const expect = require('chai').expect;
 const request = require('supertest');
+const User = require('../models/user');
 
 const app = require('../index');
 const conn = require('../db_index');
@@ -17,10 +18,10 @@ after((done) => {
         .catch((err) => done(err));
 });
 
-describe('POST /user/', () => {
+describe('POST /user/register', () => {
 
     it('Ok, creating a new user', (done) => {
-        request(app).post('/user/')
+        request(app).post('/user/register')
             .send({ dni: 35262728, name: "Test", password: "testeando", email: "test@test.com" })
             .then((res) => {
                 const body = res.body;
@@ -35,7 +36,7 @@ describe('POST /user/', () => {
     });
 
     it('Fail, user requires dni', (done) => {
-        request(app).post('/user/')
+        request(app).post('/user/register')
             .send({ name: "Test", password: "testeando", email: "test@test.com" })
             .then((res) => {
                 const body = res.body;
@@ -45,7 +46,7 @@ describe('POST /user/', () => {
     });
 
     it('Fail, user requires name', (done) => {
-        request(app).post('/user/')
+        request(app).post('/user/register')
             .send({ dni: 35262728, password: "testeando", email: "test@test.com" })
             .then((res) => {
                 const body = res.body;
@@ -55,11 +56,39 @@ describe('POST /user/', () => {
     });
 
     it('Fail, user requires password', (done) => {
-        request(app).post('/user/')
+        request(app).post('/user/register')
             .send({ dni: 35262728, name: "test", email: "test@test.com" })
             .then((res) => {
                 const body = res.body;
-                expect(body.message).to.equal('User validation failed: name: The name is required');
+                expect(body.message).to.equal('User validation failed: password: The password is required');
+                return done();
+            }).catch((err) => done(err));
+    });
+});
+
+describe('POST /user/login', () => {
+
+    before(async function() {
+        const userLogin = new User({dni: 32323235, name: 'Login', password: 'login1234', email: 'login@email.com'});
+        await userLogin.save();
+    });
+
+    it('Ok, login a user', (done) => {
+        request(app).post('/user/login')
+            .send({ email: 'login@email.com', password: 'login1234'})
+            .then((res) => {
+                const body = res.body;
+                expect(body.message).to.equal(`The user Login is login correctly`);
+                return done();
+            }).catch((err) => done(err));
+    });
+
+    it('Fail, the user password does not correctly', (done) => {
+        request(app).post('/user/login')
+            .send({ email: "login@email.com", password: "asd1234" })
+            .then((res) => {
+                const body = res.body;
+                expect(body.message).to.equal('This password is invalid');
                 return done();
             }).catch((err) => done(err));
     });
@@ -67,7 +96,7 @@ describe('POST /user/', () => {
 
 describe('UPDATE /user/:id', () => {
 
-    it('Ok, updated a user', (done) => {
+    it('Ok, updated a user id 1', (done) => {
         request(app).put('/user/1')
             .send({ dni: 35262730, name: "Update", password: "updatedd", email: "upd@upd.com" })
             .then((res) => {
@@ -83,7 +112,7 @@ describe('UPDATE /user/:id', () => {
 
 describe('DELETE /user/:id', () => {
 
-    it('Ok, deleted a user', (done) => {
+    it('Ok, deleted a user id 1', (done) => {
         request(app).delete('/user/1')
             .then((res) => {
                 const body = res.body
